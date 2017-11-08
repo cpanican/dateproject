badc6f7fe2657b7f6126c0148aa09d62f655fd6a
Chris Panican <chrispanican@gmail.com>
2017-11-07 19:42:35 -0500

    Finished

--- dateapp/routes.py
+++ dateapp/routes.py
@@ -1,8 +1,17 @@
 from . import app
 import datetime
 from flask import request, render_template
+from datetime import datetime
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-	return render_template("index.html")
+	if request.method == 'GET':
+		day = request.args.get('date')
+		if day:
+			date_object = datetime.strptime(day, '%Y-%m-%d')
+			date_return = date_object.strftime('%A, %B %d, %Y')
+			return render_template('index.html', result=date_return)
+
+		else:
+			return render_template('index.html')
